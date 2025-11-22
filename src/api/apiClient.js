5d5a9e64be35ca5c23e16dfeabcff80408a4cc23
Priyanshu Dhakre <priyanshudhakre26@gmail.com>
2025-11-22 10:53:09 +0530

    Fix Azure API DNS URLs

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -12,10 +12,11 @@ const getAPIBase = () => {
   }
 
 return {
-  USERS: "https://users-service-func-fffretdjdmcefjet.centralus-01.azurewebsites.net/api/users",
-  ORDERS: "https://orders-service-func-hwgafmebaxc3cgc3.centralus-01.azurewebsites.net/api/orders",
-  NOTIFY: "https://notifications-service-func-a6cyavhebtfeasek.centralus-01.azurewebsites.net/api/notify"
+  USERS: "https://users-service-func.centralus.azurewebsites.net/api/users",
+  ORDERS: "https://orders-service-func.centralus.azurewebsites.net/api/orders",
+  NOTIFY: "https://notifications-service-func.centralus.azurewebsites.net/api/notify"
 };
+
 };
 
 export const API_BASE = getAPIBase();
